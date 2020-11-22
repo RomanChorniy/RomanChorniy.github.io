79ab310e2ad6ccc10a0a7647c6623258ca952cf6
Roman Chornyi <rblackua@gmail.com>
2020-11-23 00:41:22 +0200

    Alta-travel. Main page. Mobile. Add search btn funkctionality.

--- Projects/alfa-travel/styles/js/index.js
+++ Projects/alfa-travel/styles/js/index.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
     $('.menuBar__telephoneBtn').click(function () {
         $('.telephoneWindow').addClass('telephoneWindow_openStatus');
+        $('.searchWindow').removeClass('searchWindow_openStatus');
     });
     $(".telephoneWindow__closeBtn").click(function () {
+        $('.telephoneWindow').removeClass('telephoneWindow_openStatus');
+    });
+    $('.menuBar__searchBtn').click(function () {
+        if ($('.searchWindow').hasClass('searchWindow_openStatus')) $('.searchWindow').removeClass('searchWindow_openStatus')
+        else $('.searchWindow').addClass('searchWindow_openStatus');
         $('.telephoneWindow').removeClass('telephoneWindow_openStatus');
     });
 });

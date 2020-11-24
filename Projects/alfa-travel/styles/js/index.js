07f3a4cba26e406be995af31f2f6f0b7cc520574
Roman Chornyi <rblackua@gmail.com>
2020-11-24 16:38:27 +0200

    Alta-travel. Main page. Mobile. add acordionMenu

--- Projects/alfa-travel/styles/js/index.js
+++ Projects/alfa-travel/styles/js/index.js
@@ -19,4 +19,38 @@ $(document).ready(function () {
         if ($('.secondLangItem').hasClass('openBlock')) $('.languageBox__indicator').addClass('rotateIndecator')
         else $('.languageBox__indicator').removeClass('rotateIndecator')
     });
+    $('.menuWindowContainer__accordionMenu .head').click(function () {
+        var head = $(this);
+
+
+        // remove any active head
+        head.siblings('.head').removeClass('active');
+
+
+
+        var section = head.next('.section');
+        //remove .section to exclude from hide all
+        section.removeClass('section');
+
+
+
+        //hide sibling sections
+        head.siblings('.section').hide();
+
+
+
+        // set .section class back
+        section.addClass('section');
+
+
+
+        if (!section.css(':visible')) {
+            // set as active and show section
+            head.addClass('active');
+            section.fadeIn(500);
+        };
+
+
+
+    });
 });

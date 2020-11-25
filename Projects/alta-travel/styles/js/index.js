ed1ea16714871c55de733630e7c2fcbfd2a53177
Roman Chornyi <rblackua@gmail.com>
2020-11-25 18:04:49 +0200

    Alta-travel. Main page. headre add LinkLogo

--- Projects/alta-travel/styles/js/index.js
+++ Projects/alta-travel/styles/js/index.js
@@ -19,28 +19,33 @@ $(document).ready(function () {
         if ($('.secondLangItem').hasClass('openBlock')) $('.languageBox__indicator').addClass('rotateIndecator')
         else $('.languageBox__indicator').removeClass('rotateIndecator')
     });
+    // $('.menuWindowContainer__accordionMenu .accordionMenu__item').click(function () {
+    //     var head = $(this);
+
+    //     // remove any active head
+    //     head.siblings('.accordionMenu__item').removeClass('active');
+
+    //     var section = head.next('.section');
+    //     //remove .section to exclude from hide all
+    //     section.removeClass('section');
+
+    //     //hide sibling sections
+    //     head.siblings('.section').hide();
+
+    //     // set .section class back
+    //     section.addClass('section');
+
+    //     if (!section.css(':visible')) {
+    //         // set as active and show section
+    //         head.addClass('active');
+    //         section.fadeIn(500);
+    //         $('.accordionMenu__item_icon').removeClass('clouseIcon');
+    //     };
+    // });
     $('.menuWindowContainer__accordionMenu .accordionMenu__item').click(function () {
-        var head = $(this);
-
-        // remove any active head
-        head.siblings('.accordionMenu__item').removeClass('active');
-
-        var section = head.next('.section');
-        //remove .section to exclude from hide all
-        section.removeClass('section');
-
-        //hide sibling sections
-        head.siblings('.section').hide();
-
-        // set .section class back
-        section.addClass('section');
-
-        if (!section.css(':visible')) {
-            // set as active and show section
-            head.addClass('active');
-            section.fadeIn(500);
-            $('.accordionMenu__item_icon').removeClass('clouseIcon');
-        };
+        var myThis = $(this);
+        myThis.toggleClass('active');
+        myThis.next().toggleClass('d-block');
     });
     $('.menuBar__menuBtn').click(function () {
         $('.menuWindow').addClass('menuWindow_open');

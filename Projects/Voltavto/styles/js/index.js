745a933a16005383852af9e83644a2c10639fa90
Roman Chornyi <rblackua@gmail.com>
2021-02-11 10:57:24 +0200

    Voltavto. Main page. reviews block

--- Projects/Voltavto/styles/js/index.js
+++ Projects/Voltavto/styles/js/index.js
@@ -6,4 +6,11 @@ $(document).ready(function () {
         prevArrow: "<img src='image/prev.svg' class='prev' alt='prev arrow'>",
         nextArrow: "<img src='image/next.svg' class='next' alt='next arrow'>"
     });
+    $('.reviews__slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: "<img src='image/prev.svg' class='prev' alt='prev arrow'>",
+        nextArrow: "<img src='image/next.svg' class='next' alt='next arrow'>"
+    });
 });
